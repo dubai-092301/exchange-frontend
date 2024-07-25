eeada16093239f48a3ea9751d01d8868b29345af
sunil-reboot <sunilpareekps@gmail.com>
2024-07-25 20:16:31 +0530

    test

--- bid-app/src/components/NominateUsers/NominateUser.js
+++ bid-app/src/components/NominateUsers/NominateUser.js
@@ -22,6 +22,7 @@ export default function NominateUser() {
                                     <td>1</td>
                                     <td>2</td>
                                     <td>3</td>
+                                    
                                     <td>4</td>
                                     <td>5</td>
                                 </tr>
