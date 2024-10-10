a0cbd8045f0fc0b8c1e81ee5d7213798ad3a5816
sunil-reboot <sunilpareekps@gmail.com>
2024-10-10 23:15:15 +0530

    changes for domain

--- bid-app/src/components/LoginComponents/Login.js
+++ bid-app/src/components/LoginComponents/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
 
     const loginUser = (e) => {
         e.preventDefault();
-        axios.post('https://exchange-btc.in:8080/api/client/auth/login/', credentials)
+        axios.post('http://3.25.226.92:8080/api/client/auth/login/', credentials)
             .then(response => {
                 if (response.data.status === 'failed') {
                     alert(response.data.message);
